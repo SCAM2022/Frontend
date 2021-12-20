f00a74f776146d71d8319cde419009f357d6449d
Dezx20 <2009awektoppo@gmail.com>
2021-12-20 18:17:06 +0530

    fixed route for login/signup

--- front/src/Pages/UserDashboard/UserDashboard.jsx
+++ front/src/Pages/UserDashboard/UserDashboard.jsx
@@ -1,48 +1,52 @@
 import React from "react";
 
 //  File
-import { Routes} from "react-router-dom";
-import { Route} from "react-router";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router";
 import LoginScreen from "../../components/LoginScreen/LoginScreen";
 import Navbar from "../../components/Navbar/Navbar";
+import { useParams } from "react-router";
 // import Hero from "../../components/Hero/Hero"
-import Top from "../../components/Top/Top"
-import About from "../../components/About/About"
-import Gallery from "../../components/Gallery/Gallery"
+import Top from "../../components/Top/Top";
+import About from "../../components/About/About";
+import Gallery from "../../components/Gallery/Gallery";
 import Contact from "../../components/Contact/Contact";
-import Footer from "../../components/Footer/Footer"
-import Clubs from "../../components/Clubs/Clubs"
+import Footer from "../../components/Footer/Footer";
+import Clubs from "../../components/Clubs/Clubs";
 import PageNotFound from "../PageNotFound";
 import Signup from "../../components/SignUp/Signup";
-import Club from "../../components/Club/Club"
+import Club from "../../components/Club/Club";
 
-import Home from "../../components/Home/Home"
+import Home from "../../components/Home/Home";
+import { path } from "express/lib/application";
 const UserDashboard = (props) => {
-  return (
-   <>
-     <Navbar />
+  console.log("path->", path);
 
-     
-     <Routes>
-      <Route exact path="/login" element={<LoginScreen />} /> 
-      <Route exact path="/signup" element={<Signup />} />
-      <Route path="*" component={PageNotFound} />
-    </Routes>
-     <Routes>
-           
-           <Route exact path="/" element={<Home />} />
-           <Route exact path="/clubs" element={<Clubs />} />
-           <Route exact path="/club" element={<Club />} />
-           <Route exact path="/top" element={<Top/>} />
-           <Route exact path="/about" element={<About />} />
-           <Route exact path="/gallery" element={<Gallery />} />
-           <Route exact path="/contact" element={<Contact />} />
-         
+  return (
+    <>
+      <Routes>
+        <Route exact path="/login" element={<LoginScreen />} />
+        <Route exact path="/signup" element={<Signup />} />
       </Routes>
-        <Footer />
-       
-   </>
-  
+      {!(
+        window.location.pathname !== "/login" ||
+        window.location.pathname !== "/signup"
+      ) && <Navbar />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/clubs" element={<Clubs />} />
+        <Route exact path="/club" element={<Club />} />
+        <Route exact path="/top" element={<Top />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/gallery" element={<Gallery />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="*" component={PageNotFound} />
+      </Routes>
+      {!(
+        window.location.pathname !== "/login" ||
+        window.location.pathname !== "/signup"
+      ) && <Footer />}
+    </>
   );
 };
 
