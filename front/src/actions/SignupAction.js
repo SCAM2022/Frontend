f300606aded29675eaf5b8c96fdb6111db828832
Dezx20 <40682998+Dezx20@users.noreply.github.com>
2021-12-20 16:45:05 +0530

    Revert "new branch merging "

---
+++ front/src/actions/SignupAction.js
@@ -0,0 +1,19 @@
+export const addUserInfo = (userInfo) => {
+  return {
+    type: "SET",
+    userInfo,
+  };
+};
+
+// export const addToPageTwo = (pageTwo) => {
+//   return {
+//     type: "PAGE_TWO",
+//     pageTwo,
+//   };
+// };
+// export const addToPageThree = (pageThree) => {
+//   return {
+//     type: "PAGE_THREE",
+//     pageThree,
+//   };
+// };
--- front/src/components/Ui/LoadingSpinner/LoadingSpinner.js
+++ front/src/components/Ui/LoadingSpinner/LoadingSpinner.js
@@ -1,11 +1,7 @@
-import classes from "./LoadingSpinner.module.css";
+import classes from './LoadingSpinner.module.css';
 
 const LoadingSpinner = () => {
-  return (
-    <div className={classes.spinner}>
-      <h2>Loading....</h2>
-    </div>
-  );
-};
+  return <div className={classes.spinner}></div>;
+}
 
 export default LoadingSpinner;
