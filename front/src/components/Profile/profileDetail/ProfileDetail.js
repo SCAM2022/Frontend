94ab352ec691398a332eec0c651f2a311fa5ca43
Dezx20 <2009awektoppo@gmail.com>
2021-12-24 18:09:38 +0530

    few changes in ui of profile

--- front/src/components/Profile/profileDetail/ProfileDetail.js
+++ front/src/components/Profile/profileDetail/ProfileDetail.js
@@ -2,51 +2,53 @@ import React from "react";
 import "./ProfileDetail.css";
 import { ProfileInfo } from "../ProfileInfo";
 
-const ProfileDetail = () => {
+const ProfileDetail = ({ userData, ...props }) => {
   return (
     <div className="profileDetail">
-      {ProfileInfo.map((detail) => {
-        return (
-          <>
-            <div className="profileDetail_name profileDetail_info">
-              <span>userName :</span>
-              <span>{detail.name}</span>
-            </div>
-            <div className="profileDetail_branch profileDetail_info">
-              <span>Branch :</span>
-              <span>{detail.branch}</span>
-            </div>
-            <div className="profileDetail_enroll profileDetail_info">
-              <span>Enrollment No. :</span>
-              <span>{detail.enroll}</span>
-            </div>
-            <div className="profileDetail_roll profileDetail_info">
-              <span>Roll No. :</span>
-              <span>{detail.roll}</span>
-            </div>
-            <div className="profileDetail_email profileDetail_info">
-              <span>E-mail :</span>
-              <span>{detail.email}</span>
-            </div>
-            <div className="profileDetail_join profileDetail_info">
-              <span>Club joined :</span>
-              <span>{detail.join}</span>
-            </div>
-            <div className="profileDetail_last profileDetail_info">
-              <span>Last visit :</span>
-              <span>{detail.last}</span>
-            </div>
-            <div className="profileDetail_register profileDetail_info">
-              <span>Registered :</span>
-              <span>{detail.registered}</span>
-            </div>
-            <div className="profileDetail_date profileDetail_info">
-              <span>Join Date :</span>
-              <span>{detail.date}</span>
-            </div>
-          </>
-        );
-      })}
+      <div className="profileDetail_name profileDetail_info">
+        <span>userName :</span>
+        <span>{userData.name}</span>
+      </div>
+      <div className="profileDetail_branch profileDetail_info">
+        <span>Branch :</span>
+        <span>{userData.department}</span>
+      </div>
+      <div className="profileDetail_enroll profileDetail_info">
+        <span>Enrollment No. :</span>
+        <span>{userData.enroll}</span>
+      </div>
+      <div className="profileDetail_roll profileDetail_info">
+        <span>Roll No. :</span>
+        <span>{userData.rollno}</span>
+      </div>
+      <div className="profileDetail_email profileDetail_info">
+        <span>E-mail :</span>
+        <span>{userData.email}</span>
+      </div>
+      <div className="profileDetail_join profileDetail_info">
+        <span>Club joined :</span>
+        <span>{"detail.join"}</span>
+      </div>
+      <div className="profileDetail_last profileDetail_info">
+        <span>Last visit :</span>
+        <span>{"detail.last"}</span>
+      </div>
+      <div className="profileDetail_register profileDetail_info">
+        <span>Registered :</span>
+        <span>{"detail.registered"}</span>
+      </div>
+      <div className="profileDetail_date profileDetail_info">
+        <span>Join Date :</span>
+        <span>{"detail.date"}</span>
+      </div>
+      <div className="profileDetail_date profileDetail_info">
+        <span>Semester :</span>
+        <span>{userData.semester}</span>
+      </div>
+      <div className="profileDetail_date profileDetail_info">
+        <span>Admission Year :</span>
+        <span>{userData.admissionYear}</span>
+      </div>
     </div>
   );
 };
